2b92718451c80fca5c9fb9d80c7a70eed049b927
Monishasaravanan17 <monisha.saravanan@softsuave.com>
2021-11-19 14:29:16 +0530

    second commit

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ app.post("/api/register", (req, res) => {
 // ----------------until here--------------
 
 
-// -----------------working register api--------------
+// -----------------without promise register api--------------
 
 
 // app.post("/api/register", (req, res) => {
@@ -89,129 +89,6 @@ app.post("/api/register", (req, res) => {
 // });
 
 // ---------------------------untill here------------------------
-
-// app.post("/api/register", (req, res) => {
-//     const firstname = req.body.firstname;
-//     const lastname = req.body.lastname;
-//     const email = req.body.email;
-//     const password = req.body.password;
-//     const salt = 10;
-//     const encryptedPassword = bcrypt.hashSync(password, salt);
-//     asyncCall();
-//     function resolveAfter5Seconds() {
-//         setTimeout(() => {
-//             db.query("SELECT * FROM regtable WHERE email = ?", [email], (err, result) => {
-//                 if (err) {
-//                     res.send(err)
-//                 }
-//                 console.log(result)
-//                 if (result.length > 0) {
-//                     return ( "you already have an account,please login" );
-//                 }
-//                 db.query("INSERT INTO regtable (firstname,lastname,email,password) VALUES (?,?,?,?)", [firstname, lastname, email, encryptedPassword], (err, res) => {
-//                     if (err) {
-//                         return res.send(err);
-//                     } else {
-//                         return ( "Registered successfully" );
-//                     }
-//                 })
-//             })
-//         }, 5000);
-//     }
-
-//     async function asyncCall() {
-//         res.send("loading...")
-//         console.log('loading');
-//         return await resolveAfter5Seconds();
-
-//     }
-//     console.log("register api hitted...")
-// });
-
-//     const promise = new promise(function (ressolve,reject)  {
-//         db.query("SELECT * FROM regtable WHERE email = ?" [email],(err,res) =>{
-//             if(err){
-//                 res.send (err)
-//             }
-//         })
-//     });
-//     const getdata = async()=> {
-//         try{
-// return await res.send({message:"user already exist,login please"})
-//         }
-//         catch{
-
-//         }
-//     }
-// async function promise(success, failure) {
-
-//     db.query("SELECT * FROM regtable WHERE email = ?", [email], (err, result) => {
-//         if (err) {
-//             return res.send(err);
-//         }
-//         if (result[0]) {
-//             return await({ message: "user already exist, please login" });
-//         } else {
-//             db.query("INSERT INTO regtable (firstname,lastname,email,password) VALUES (?,?,?,?)", [firstname, lastname, email, encryptedPassword], (err, result) => {
-//                 if (err) {
-//                     return await(err)
-//                 } else {
-//                     return await({ message: "registered successfully" })
-//                 }
-
-//             })
-//         }
-//     })
-
-// }.then(success)
-
-// async function app() {
-
-//     db.query("SELECT * FROM regtable WHERE email = ?", [email], (err, result) => {
-//         if (err) {
-//             return res.send(err);
-//         }
-//         console.log("result", result);
-//         return await(success)
-//     }, 5000).then((success) => {
-//         if (result[0]) {
-//             return res.send({ message: "user already exists,please login" });
-//         } else {
-//             db.query("INSERT INTO regtable(firstname,lastname,email,password) VALUES (?,?,?,?)", [firstname, lastname, email, encryptedPassword], (err, result) => {
-//                 if (err) {
-//                     return res.send(err);
-//                 } else {
-//                     return res.send({ message: "registered successfull" });
-//                 }
-//             })
-//         }
-
-//     }).catch((e) => {
-//         return res.send(err)
-//     });
-// }
-
-// setTimeout(function () {
-//     const message = res.send({ message: "user already exists,please login" });
-
-//     console.log("time delayed...")
-//     db.query("SELECT * FROM regtable WHERE email = ?", [email], (err, result) => {
-//         if (err) {
-//             return res.send(err)
-//         }
-
-//         if (result.length > 0) {
-//             return message;
-//         }
-//     })
-//     db.query("INSERT INTO regtable(firstname,lastname,email,password) VALUES (?,?,?,?)", [firstname, lastname, email, encryptedPassword], (err, result) => {
-//         if (err) {
-//             return res.send(err);
-//         } else {
-//             return res.send({ message: "registered successfull" });
-//         }
-//     })
-// }, 5000)
 
 
 
